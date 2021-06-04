61345db9aece17140f721e6eb3942879b187a6f0
Arm7x2 <alexdevduell@gmail.com>
2021-06-04 06:20:18 -0400

    Initial Pages

---
+++ src/index.php
@@ -0,0 +1,31 @@
+<?php
+// Simple-Wallet Created by ARM7x2
+include("include/easybtc.php");
+
+
+
+// Initiate a connection with your bitcoin RPC Server
+$bitcoin = new Bitcoin('username','password','localhost','8332');
+
+// Enable SSL if you choose
+// $bitcoin->setSSL('/full/path/to/mycertificate.cert');
+
+
+$username = "MyUsername";
+//Create a wallet for username if it hasnt been created
+
+?>
+<html>
+    <head>
+        <title>SimpleWallet</title>
+        <script src="css/layout.css" type="text/javascript">
+        
+    </head>
+    <body>
+        <div class="maincontent">
+            
+            
+        </div>
+        
+    </body>
+</html>
